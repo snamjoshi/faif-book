a58cc12f25153a1963ff4de042605e2b12177e74
Sanjeev Namjoshi <snamjoshi87@utexas.edu>
2024-10-10 11:11:33 -0500

    refactoring generative model file

--- src/generative_models.py
+++ src/generative_models.py
@@ -3,6 +3,9 @@ import numpy as np
 from scipy.stats import norm
 from typing import Union
 
+def invert_generative_model():
+    ...
+
 def linear_gaussian(y: float, x: Union[float, np.ndarray], 
                     mu_y: callable, std_y: float, 
                     m_x: float, s_x: float):
