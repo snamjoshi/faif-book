7afbe0799b65421e02cde771c186a5cf25a2cfd7
Sanjeev Namjoshi <snamjoshi87@utexas.edu>
2024-10-11 17:02:41 -0500

    linear regression notebook

--- src/analytic.py
+++ src/analytic.py
@@ -7,3 +7,9 @@ def mle_hidden_state(y: float, beta_0: float, beta_1: float) -> Union[float, np.
 
 def map_hidden_state(y: float, beta_0: float, beta_1: float, m_x: float) -> Union[float, np.ndarray]:
     return (beta_1 * (np.mean(y) - beta_0) + m_x) / (beta_1**2 + 1)
+
+def mle_beta_1(x: float, y: float) -> float:
+    return np.cov(x, y)[0][1] / np.var(x, ddof=1)
+
+def mle_beta_0(x: float, y: float, beta_1: float) -> float:
+    return np.mean(y) - beta_1 * np.mean(x)
